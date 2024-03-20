e11d7bdd73ed99c95e07f4c55748900da469e549
Christian Schmitz <cschmitz398@gmail.com>
2024-03-20 16:56:01 -0600

    manual coding of all declarations

--- src/global.d.ts
+++
@@ -1,5 +0,0 @@
-export {}
-
-declare global {
-    type Option<T> = null | undefined | T
-}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
-export { isNone, isSome, None } from "./option.js"
+/**
+ * @type {null}
+ */
+export const None = null
+
+/**
+ * @template T
+ * @param {Option<T>} opt
+ * @returns {opt is T}
+ */
+export function isSome(opt) {
+    return opt !== null && opt !== undefined
+}
+
+/**
+ * @template T
+ * @param {Option<T>} opt
+ * @returns {opt is (null | undefined)}
+ */
+export function isNone(opt) {
+    return opt === null || opt === undefined
+}
